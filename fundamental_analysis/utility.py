20fb282ac2ac3f1391ea0cf9236e7882f6e7f5b9
forrest.li <forrest.shijili@gmail.com>
2024-11-10 11:53:51 +0800

    add more docstring & black format

--- fundamental_analysis/utility.py
+++ fundamental_analysis/utility.py
@@ -4,6 +4,7 @@ import sqlalchemy
 import yfinance as yf
 import numpy as np
 import pandas_ta as pandas_ta
+import pandas as pd
 
 
 def get_data(tickers):
@@ -14,40 +15,104 @@ def get_data(tickers):
 
 
 def create_engine(name):
-    engine = sqlalchemy.create_engine('sqlite:///' + name + '.db')
+    engine = sqlalchemy.create_engine("sqlite:///" + name + ".db")
     return engine
 
 
 def TOSQL(frames, engine):
     for symbol, frame in frames:
         frame.to_sql(symbol, engine.raw_connection(), index=False, if_exists="replace")
-    logger.info('imported successfully')
+    logger.info("imported successfully")
+
+
+def check_pandas(df: pd.DataFrame):
+    logger.info("Process started for function: get_return..")
+    if df.empty:
+        logger.debug("The dataframe is empty. No transformations will be applied.")
+        return df
+    logger.info("Applying transformations to pd")
+
+
+def get_return(df: pd.DataFrame):
+    """
+    :param data_frame: Pandas DataFrame as Input
+
+    :returns:
+    data_frame: Transformed Pandas DataFrame as Output
+    """
+    check_pandas(df)
+    df["% Change"] = round(df["Adj Close"] / df["Adj Close"].shift(1) - 1, 4)
+    logger.info("Process completed for function: get_return..")
+    return df
 
 
 def get_garman_klass_vol(df):
-    df['garman_klass_vol'] = ((np.log(df['high']) - np.log(df['low'])) ** 2) / 2 - (2 * np.log(2) - 1) * (
-            (np.log(df['adj close']) - np.log(df['open'])) ** 2)
+    """
+    :param data_frame: Pandas DataFrame as Input
+
+    :returns:
+    data_frame: Transformed Pandas DataFrame as Output
+    """
+    check_pandas(df)
+    df["garman_klass_vol"] = ((np.log(df["high"]) - np.log(df["low"])) ** 2) / 2 - (
+        2 * np.log(2) - 1
+    ) * ((np.log(df["adj close"]) - np.log(df["open"])) ** 2)
+    logger.info("Process completed for function: get_garman_klass_vol..")
     return df
 
 
 def get_rsi(df):
-    df['rsi'] = df.groupby(level=1)['adj close'].transform(lambda x: pandas_ta.rsi(close=x, length=20))
+    """
+    :param data_frame: Pandas DataFrame as Input
+
+    :returns:
+    data_frame: Transformed Pandas DataFrame as Output
+    """
+    check_pandas(df)
+    df["rsi"] = df.groupby(level=1)["adj close"].transform(
+        lambda x: pandas_ta.rsi(close=x, length=20)
+    )
+    logger.info("Process completed for function: get_rsi..")
     return df
 
 
 def get_adj_close(df):
-    df['bb_low'] = df.groupby(level=1)['adj close'].transform(
-        lambda x: pandas_ta.bbands(close=np.log1p(x), length=20).iloc[:, 0])
+    """
+    :param data_frame: Pandas DataFrame as Input
+
+    :returns:
+    data_frame: Transformed Pandas DataFrame as Output
+    """
+    check_pandas(df)
+    df["bb_low"] = df.groupby(level=1)["adj close"].transform(
+        lambda x: pandas_ta.bbands(close=np.log1p(x), length=20).iloc[:, 0]
+    )
     return df
 
 
 def get_bb_mid(df):
-    df['bb_mid'] = df.groupby(level=1)['adj close'].transform(
-        lambda x: pandas_ta.bbands(close=np.log1p(x), length=20).iloc[:, 1])
+    """
+    :param data_frame: Pandas DataFrame as Input
+
+    :returns:
+    data_frame: Transformed Pandas DataFrame as Output
+    """
+    check_pandas(df)
+    df["bb_mid"] = df.groupby(level=1)["adj close"].transform(
+        lambda x: pandas_ta.bbands(close=np.log1p(x), length=20).iloc[:, 1]
+    )
     return df
 
 
 def get_bb_high(df):
-    df['bb_high'] = df.groupby(level=1)['adj close'].transform(
-        lambda x: pandas_ta.bbands(close=np.log1p(x), length=20).iloc[:, 2])
+    """
+    :param data_frame: Pandas DataFrame as Input
+
+    :returns:
+    data_frame: Transformed Pandas DataFrame as Output
+    """
+    check_pandas(df)
+    df["bb_high"] = df.groupby(level=1)["adj close"].transform(
+        lambda x: pandas_ta.bbands(close=np.log1p(x), length=20).iloc[:, 2]
+    )
     return df
